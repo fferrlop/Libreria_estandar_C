7a5e8f14bfb1cb278aa4c6d62a8d4b7131495c5c
fferrlop <fferrlop@myuax.com>
2023-12-18 16:14:10 +0100

    main3a

--- main2.c
+++ main2.c
@@ -7,6 +7,8 @@
 
 
 int main() {
+
+//2. Cadenas
     char str1[] = "Hola";
     char str2[] = "Mundo";
 
@@ -23,6 +25,17 @@ int main() {
     int length = strlen(str);
 
     printf("La longitud de '%s' es: %d\n", str, length);
+
+//3. Archivos
+
+    FILE *file = fopen("file.txt", "r");
+    if (file == NULL) {
+        printf("El archivo no se pudo abrir.\n");
+        return 1;
+    }
+
+    // Cerrar el archivo al final
+    fclose(file);
 
     return 0;
 }
