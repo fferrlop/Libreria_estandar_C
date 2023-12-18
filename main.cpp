0741dc007796c9ed7548c661a8430b60cb308dd3
fferrlop <fferrlop@myuax.com>
2023-12-18 15:46:20 +0100

    b. main

--- main.cpp
+++ main.cpp
@@ -4,11 +4,20 @@
 #include <time.h>
 
 int main() {
+
+//1. Funciones comunes
+//a)
     int num = -10;
 
     int abs_num = abs(num);
 
     printf("El valor absoluto de %d es %d\n", num, abs_num);
+
+//b)
+    srand(time(0));
+
+    int random_number = rand();
+    printf("Número aleatorio: %d\n", random_number);
 
     return 0;
 }
